7b48d32292f3c9b103cb752c7aed77c604e212b7
Nifemi Alpine <femi@nifemi-alpine.local>
2020-07-21 10:22:46 +0100

    docker

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 docker pull oksuz/tcp-proxy
-docker run --name my-tcp-proxy -p43666:5000 -eREMOTE_HOST=127.0.0.1 -eREMOTE_PORT=43666 -d oksuz/tcp-proxy
+docker run --name my-tcp-proxy -p43666:5000 -eREMOTE_HOST=10.154.0.12 -eREMOTE_PORT=43666 -d oksuz/tcp-proxy
